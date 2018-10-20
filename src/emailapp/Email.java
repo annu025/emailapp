14c1773f0fc24ea211b122f6af147a1b1efbf30a
annu025 <anundhara.c@gmail.com>
2018-10-19 21:27:12 -0700

    Accepting user input for department code

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,9 +15,24 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Email created by: " + this.firstName + " "  + this.lastName);
+
+        // Call a method to ask for department
+        // Method return the department value
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
     }
     // Ask for department (accounts, sales..)
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODES: \n1. Sales \n2. Accounting \n3. Development \n0. None \nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
 
+        if(departmentChoice == 1){ return "Sales"; }
+        else if(departmentChoice == 2){ return "Accounting"; }
+        else if(departmentChoice == 3){ return "Development"; }
+        else {return "None"; }
+    }
     // Generate a random password
 
     // Set mailbox capacity
